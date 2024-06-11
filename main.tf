2999bbc436353c085c1e3de763d7370a4606b298
Alex Nauda <alex@nobl9.com>
2024-06-11 11:31:21 -0400

    specific setup for AWS workshop

--- main.tf
+++ main.tf
@@ -11,16 +11,14 @@ data "aws_eks_cluster" "this" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  config_path    = "~/.kube/config"
+  config_context = "PetSite"
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
+    config_path    = "~/.kube/config"
+    config_context = "PetSite"
   }
 }
 
